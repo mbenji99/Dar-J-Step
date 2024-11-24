2f20b8d4f80e90e52882747a97765f92c5403ce4
Stephanie Effs <stephanieeffs0205@gmail.com>
2024-11-24 02:11:03 -0500

    change scheduleroutes

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const scheduleController = require("../controllers/scheduleController");
 
 // Define routes
-router.put("/:id/:day", scheduleController.assignShiftToDay); // ASSIGN day of week to shift
-router.get("/", scheduleController.getAllSchedules); // GET all schedules
-router.get("/:id", scheduleController.getScheduleById); // GET a schedule by ID
-router.post("/schedule", scheduleController.createSchedule); // CREATE a new schedule
-router.put("/:id", scheduleController.editSchedule); // EDIT a schedule
-router.delete("/:id", scheduleController.deleteSchedule); // DELETE a schedule
+router.put("/assign-shift-to-day", scheduleController.assignShiftToDay); // ASSIGN day of week to shift
+router.get("/get-all-schedules", scheduleController.getAllSchedules); // GET all schedules
+router.get("/get-schedule-by-id", scheduleController.getScheduleById); // GET a schedule by ID
+router.post("/create-schedule", scheduleController.createSchedule); // CREATE a new schedule
+router.put("/edit-schedule", scheduleController.editSchedule); // EDIT a schedule
+router.delete("/delete-schedule", scheduleController.deleteSchedule); // DELETE a schedule
 
 module.exports = router;
