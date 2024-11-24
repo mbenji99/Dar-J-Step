f5d2d81c1571964790d83fbcae37c5bfbf431c54
Stephanie Effs <stephanieeffs0205@gmail.com>
2024-11-24 01:04:56 -0500

    route name change

--- backend/routes/shiftRoutes.js
+++ backend/routes/shiftRoutes.js
@@ -5,5 +5,6 @@ const shiftController = require('../controllers/shiftController');
 router.post('/create-shift',shiftController.createShift);
 router.get('/view-shifts',shiftController.viewShifts);
 router.get('/delete-shift',shiftController.deleteShift);
+router.post('/edit-shift',shiftController.editShift);
 
 module.exports = router;
